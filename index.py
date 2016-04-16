03ba0ae40df24a441a1008d52e7e5a24c0560cf4
John Tucker <john@larkintuckerllc.com>
2016-04-16 22:25:15 +0000

    Python done.

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
+import sys
+import time
+import Adafruit_MPR121.MPR121 as MPR121
 from socketIO_client import SocketIO, LoggingNamespace
-
-def handleResponse(*args):
-  print('MSG: ', args);
-
 with SocketIO('localhost', 3000, LoggingNamespace) as socketIO:
-  socketIO.on('response', handleResponse)
-  socketIO.emit('message', 'hello client')
-  socketIO.wait(seconds=30)
+	cap = MPR121.MPR121()
+	if not cap.begin():
+	    print('Error initializing MPR121.  Check your wiring!')
+	    sys.exit(1)
+	last_touched = cap.touched()
+	while True:
+	    current_touched = cap.touched()
+	    for i in range(12):
+		pin_bit = 1 << i
+		if current_touched & pin_bit and not last_touched & pin_bit:
+                    socketIO.emit('message', '{{"point": {0}, "action": "touched"}}'.format(i))
+		if not current_touched & pin_bit and last_touched & pin_bit:
+                    socketIO.emit('message', '{{"point": {0}, "action": "released"}}'.format(i))
+	    last_touched = current_touched
+	    time.sleep(0.1)
+
