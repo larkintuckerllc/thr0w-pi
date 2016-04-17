402af6779efbd3a56bc124f921daf77a5119ebf3
John Tucker <john@larkintuckerllc.com>
2016-04-17 01:03:52 +0000

    mostly done

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var thr0w = require('thr0w-api');
+thr0w.setBase('192.168.1.19');
+thr0w.login('admin', 'monkey', handleLogin);
+function handleLogin(error) {
+  if (error) {
+    process.exit(1);
+  }
+// TODO: INDENT
 io.on('connection', handleConnection);
 http.listen(3000);
 function handleConnection(socket) {
   socket.on('message', handleMessage);
-  function handleMessage(msg) {
-    console.log('MSG: ' + msg);
-    socket.emit('response', 'hello server');
+  function handleMessage(data) {
+    var message = JSON.parse(data);
+    thr0w.thr0w([0], message);
   }
 }
+}
+
+
